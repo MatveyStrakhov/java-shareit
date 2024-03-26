9e701f29e2eaf0d9c7dd6685464ca6c3fd09af02
MStrakhov <matveystrakhov@yandex.ru>
2024-03-26 11:36:10 +0300

    fix: added Service annotation for gateway clients

--- gateway/src/main/java/ru.practicum.shareit/request/ItemRequestClient.java
+++ gateway/src/main/java/ru.practicum.shareit/request/ItemRequestClient.java
@@ -5,12 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.shareit.client.BaseClient;
 import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 
 import java.util.Map;
 
+@Service
 public class ItemRequestClient extends BaseClient {
     private static final String API_PREFIX = "/requests";
 
